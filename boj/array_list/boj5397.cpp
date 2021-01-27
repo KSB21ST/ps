1f2a30b645341c9af40a3a8612eae477763d6e94
KSB21ST <21supersoo@kaist.ac.kr>
2021-01-27 18:32:00 +0900

    boj5397

--- boj/array_list/boj5397.cpp
+++ boj/array_list/boj5397.cpp
@@ -6,5 +6,44 @@ int main(){
     cin.tie(0);
     cout.tie(0);
 
+    int n;
+    cin >> n;
+    for(int j = 0;j<n;j++){
+        string s;
+        cin >> s;
+        list<char>ans;
 
+        auto pointer = ans.end();
+        for(auto k:s){
+            // cout << "k : " << k << endl;
+            if(k == '<'){
+                // cout<<"endter <"<<endl;
+                if(pointer != ans.begin()){
+                    pointer--;
+                }
+                // cout << "finish <" << endl;
+            }
+            else if(k == '>'){
+                // cout<<"endter >"<<endl;
+                if(pointer != ans.end()){pointer++;}
+                // cout << "finish >" << endl;
+            }
+            else if(k == '-'){
+                // cout<<"endter -"<<endl;
+                if(pointer != ans.begin()){
+                    pointer--;
+                    pointer = ans.erase(pointer);
+                }
+                // cout << "finish -" << endl;
+            }
+            else{
+                // cout<<"endter else"<<endl;
+                ans.insert(pointer, k);
+                // cout << "finish else" << endl;
+            }
+            // cout << "ans : " ;
+        }
+        for(auto in: ans){cout << in;}
+        cout << "\n";
+    }
 }
